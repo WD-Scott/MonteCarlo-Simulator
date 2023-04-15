1fbb39e37d8ce5d58c2005051c25043e75d151b0
WD-Scott <122546418+WD-Scott@users.noreply.github.com>
2023-04-15 16:09:33 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Montecarlo Simulator',
+     version='1.0.0',
+     url='',
+     description='Monte Carlo Module including Die, Game, and Analyzer',
+     author = 'Wyatt Scott',
+     author_email = 'bdr6qz@virginia.edu',
+     license='MIT',
+     packages=['montecarlo'],
+     install_requires=['numpy', 'pandas'])
